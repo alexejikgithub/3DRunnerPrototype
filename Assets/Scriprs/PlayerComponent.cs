678f5c08a373ba04629ba7a804883f0a0d4e4d6f
alexejikgithub <62622414+alexejikgithub@users.noreply.github.com>
2022-05-28 21:20:06 +0300

    Finalized movement

--- Assets/Scriprs/PlayerComponent.cs
+++ Assets/Scriprs/PlayerComponent.cs
@@ -9,15 +9,20 @@ public class PlayerComponent : MonoBehaviour
 	[SerializeField] private float _inputScale;
 	[SerializeField] private float _leftBorder;
 	[SerializeField] private float _rightBorder;
-
+	[Space]
 	[SerializeField] private Animator _animator;
 	[SerializeField] private InteractableCanvasObject _input;
 
 	private Rigidbody _rigidBody;
 
+
+
 	private bool _isRunning;
 	private float _targetX;
 	private float _previousDragX;
+	private float _previousCameraX;
+	private float _currentCameraX;
+	private float _cameraDelta;
 	private static readonly int IsRunning = Animator.StringToHash("IsRunning");
 
 	public Action<int> OnCollectCoin;
@@ -28,7 +33,7 @@ public class PlayerComponent : MonoBehaviour
 		get => _targetX;
 		set
 		{
-			_targetX= Mathf.Clamp(value, _leftBorder, _rightBorder);
+			_targetX = Mathf.Clamp(value, _leftBorder, _rightBorder);
 		}
 	}
 
@@ -55,13 +60,16 @@ public class PlayerComponent : MonoBehaviour
 	{
 		StartRunning();
 		_previousDragX = position.x;
-		
+		_previousCameraX = Camera.main.transform.position.x;
 	}
 
 	private void Drag(Vector3 position)
 	{
-		TargetX = _previousDragX + (position.x - _previousDragX)* _inputScale;
+		_currentCameraX = Camera.main.transform.position.x;
+		_cameraDelta = _currentCameraX - _previousCameraX;
+		TargetX = TargetX + (position.x - _cameraDelta - _previousDragX) * _inputScale;
 		_previousDragX = position.x;
+		_previousCameraX = Camera.main.transform.position.x;
 	}
 
 	private void EndInput(Vector3 position)
