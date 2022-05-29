9ffb5665dbc67ab8395ac6461c40373bdc53c404
alexejikgithub <62622414+alexejikgithub@users.noreply.github.com>
2022-05-29 21:39:52 +0300

    Get pool items from children

--- Assets/Scripts/Pool/ObjectPoolController.cs
+++ Assets/Scripts/Pool/ObjectPoolController.cs
@@ -1,17 +1,26 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Scripts.Pool
 {
     public class ObjectPoolController<T> : MonoBehaviour where T : MonoBehaviour
     {
-        [SerializeField] private List<T> _poolObjects;
+       
         [SerializeField] private T prefabToPool;
 
+        [SerializeField] private List<T> _poolObjects;
 
         private T _currentElement;
 
         public T PrefabToPool => prefabToPool;
+
+
+        private void Awake()
+        {
+            _poolObjects=GetComponentsInChildren<T>(true).ToList();
+        }
 
         public T GetPooledGameObject()
         {
